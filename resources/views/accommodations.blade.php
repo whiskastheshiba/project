73b81c985e9da8c13d7b9019335a8c858ecd27a2
KatyaKuzmina <84267401+KatyaKuzmina@users.noreply.github.com>
2021-06-13 13:19:13 +0300

    Add files via upload

---
+++ resources/views/accommodations.blade.php
@@ -0,0 +1,41 @@
+
+
+
+
+@extends('layouts.app')
+@section('content')
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+<div class="container">
+    <div class="row">
+        <img src="{{  url('/images/wallpaper.jpg') }}" width="100%" height="300px" alt="foto" >
+        @if (count($accommodations)==0)
+<p color='red'> There are no records in the database!</p>
+@else
+<table style="border: 1px solid black">
+<tr>
+  <td> accommodation_id </td>
+<td> accommodation_name </td>
+<td> accommodation_city </td>
+<td> accommodation_address </td>
+<td> accommodation_price </td>
+<td> accommmodation_description </td>
+<td> </td>
+</tr>
+@foreach ($accommodations as $accommodation)
+<tr>
+<td> {{ $accommodation->id }} </td>
+<td> {{ $accommodation->accommodation_name }} </td>
+<td> {{ $accommodation->accommodation_city }} </td>
+<td> {{ $accommodation->accommodation_address }} </td>
+<td> {{ $accommodation->accommodation_price }} </td>
+<td> {{ $accommodation->accommmodation_description }} </td>
+ <td> </td>
+</td>
+@endforeach
+</table>
+@endif
+<p> <input type="button" value="New Accommodation"> </p>
+    </div>
+</div>
+@endsection
